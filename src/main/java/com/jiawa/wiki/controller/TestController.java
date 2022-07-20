9f8010ccd48bc80ef9b7134ce81fdebac01b7a02
EchoChao <EchoChao@126.com>
2022-07-20 15:53:40 +0800

    2-7 增加hello接口测试test.http

---
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.jiawa.wiki.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by echo on 2022/7/20 11:57
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/hello/{id}",method = {RequestMethod.GET,RequestMethod.POST})
+    public String hello(@PathVariable String id){
+        return "Hello World!"+id;
+    }
+
+}
