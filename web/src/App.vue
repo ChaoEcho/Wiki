67599954f90c601fd52c38ac9d0fc637ecbc9628
EchoChao <EchoChao@126.com>
2022-07-21 14:50:30 +0800

    4-7 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,40 +2,40 @@
   <a-layout>
     <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      甲蛙电子书
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
   import {defineComponent} from 'vue';
   import TheHeader from '@/components/the-header'
+  import TheFooter from '@/components/the-footer'
 
   export default defineComponent({
     name: 'app',
     components:{
-      TheHeader
+      TheHeader,
+      TheFooter
     }
   });
 
 </script>
 
 <style>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
+  #components-layout-demo-top-side-2 .logo {
+    float: left;
+    width: 120px;
+    height: 31px;
+    margin: 16px 24px 16px 0;
+    background: rgba(255, 255, 255, 0.3);
+  }
 
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
+  .ant-row-rtl #components-layout-demo-top-side-2 .logo {
+    float: right;
+    margin: 16px 0 16px 24px;
+  }
 
-.site-layout-background {
-  background: #fff;
-}
+  .site-layout-background {
+    background: #fff;
+  }
 </style>
