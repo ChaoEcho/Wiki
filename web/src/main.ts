4ee82c9f52b9a68eff330d3e5c70dd11fd224b78
EchoChao <EchoChao@126.com>
2022-07-21 16:37:42 +0800

    5-6 配置axios拦截器，打印请求参数和返回参数

--- web/src/main.ts
+++ web/src/main.ts
@@ -7,7 +7,25 @@ import 'ant-design-vue/dist/antd.css'
 import * as Icons from '@ant-design/icons-vue'
 import axios from "axios";
 
+//设置baseURL
 axios.defaults.baseURL=process.env.VUE_APP_SERVER
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
 
 const app = createApp(App);
 app.use(store).use(router).use(Antd)
@@ -19,5 +37,6 @@ for (const i in icons) {
     app.component(i, icons[i]);
 }
 
+//输出环境以及服务端
 console.log('环境：'+process.env.NODE_ENV);
 console.log('服务端：'+process.env.VUE_APP_SERVER);
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -91,16 +91,12 @@ for (let i = 0; i < 23; i++) {
 export default defineComponent({
   name: 'Home',
   setup() {
-    console.log("setup")
-
     const ebooks = ref();
 
     onMounted(() => {
-      console.log("onMounted")
       axios.get("/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
-        console.log(response);
       })
     });
 
