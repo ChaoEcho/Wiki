4f0bde110d41c8020534f97a8205d79dfa757401
EchoChao <EchoChao@126.com>
2022-07-22 21:21:43 +0800

    6-4 集成PageHelper插件

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.jiawa.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiawa.wiki.domain.Ebook;
 import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
 import com.jiawa.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -17,11 +21,15 @@ import java.util.List;
  */
 @Service
 public class EbookService {
+    
+    private static final Logger LOG= LoggerFactory.getLogger(EbookService.class);
 
     @Autowired
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req) {
+
+
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
 
@@ -29,7 +37,15 @@ public class EbookService {
             criteria.andNameLike("%"+req.getName()+"%");
         }
 
+        /**
+         * 只对第一个SQL语句有效
+         */
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo=new PageInfo<>(ebookList);
+        LOG.info("总行数："+pageInfo.getTotal());
+        LOG.info("总页数："+pageInfo.getPages());
+
         List<EbookResp> ebookRespList = CopyUtil.copyList(ebookList, EbookResp.class);
         return ebookRespList;
     }
