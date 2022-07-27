3a11d9be4f83c642dab9326c8b7902a23364b471
EchoChao <EchoChao@126.com>
2022-07-27 11:37:40 +0800

    9-13 前端未登录，隐藏菜单

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo"/>
+    <div class="logo">甲蛙知识库</div>
     <a-menu
         theme="dark"
         mode="horizontal"
@@ -9,21 +9,18 @@
       <a-menu-item key="/">
         <router-link to="/">首页</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/user">
+      <a-menu-item key="/admin/user" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/user">用户管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/ebook">
+      <a-menu-item key="/admin/ebook" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/ebook">电子书管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/category">
+      <a-menu-item key="/admin/category" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/category">分类管理</router-link>
       </a-menu-item>
       <a-menu-item key="/about">
-        <router-link to="/about">About us</router-link>
+        <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" v-show="user.id" @click="showLoginModal">
-        Hello: {{ user.name }}
-      </a>
       <a-popconfirm
           title="确认退出登录?"
           ok-text="是"
@@ -34,6 +31,9 @@
           <span>退出登录</span>
         </a>
       </a-popconfirm>
+      <a class="login-menu" v-show="user.id">
+        <span>您好：{{user.name}}</span>
+      </a>
       <a class="login-menu" v-show="!user.id" @click="showLoginModal">
         <span>登录</span>
       </a>
@@ -47,10 +47,10 @@
     >
       <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
         <a-form-item label="登录名">
-          <a-input v-model:value="loginUser.loginName"/>
+          <a-input v-model:value="loginUser.loginName" />
         </a-form-item>
         <a-form-item label="密码">
-          <a-input v-model:value="loginUser.password" type="password"/>
+          <a-input v-model:value="loginUser.password" type="password" />
         </a-form-item>
       </a-form>
     </a-modal>
@@ -58,9 +58,9 @@
 </template>
 
 <script lang="ts">
-import {computed, defineComponent, ref} from 'vue';
+import { defineComponent, ref, computed } from 'vue';
 import axios from 'axios';
-import {message} from "ant-design-vue";
+import { message } from 'ant-design-vue';
 import store from "@/store";
 
 declare let hexMd5: any;
@@ -68,20 +68,21 @@ declare let KEY: any;
 
 export default defineComponent({
   name: 'the-header',
-  setup() {
-    // Save username once login
+  setup () {
+    // 登录后保存
     const user = computed(() => store.state.user);
 
-    // To login
+    // 用来登录
     const loginUser = ref({
       loginName: "test",
-      password: "123456"
+      password: "test"
     });
     const loginModalVisible = ref(false);
     const loginModalLoading = ref(false);
     const showLoginModal = () => {
       loginModalVisible.value = true;
     };
+
     // 登录
     const login = () => {
       console.log("开始登录");
@@ -93,15 +94,17 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登录成功！");
-          store.commit("setUser", data.content)
+
+          store.commit("setUser", data.content);
         } else {
           message.error(data.message);
         }
       });
     };
 
+    // 退出登录
     const logout = () => {
-      console.log("退出登录！");
+      console.log("退出登录开始");
       axios.get('/user/logout/' + user.value.token).then((response) => {
         const data = response.data;
         if (data.success) {
@@ -110,8 +113,9 @@ export default defineComponent({
         } else {
           message.error(data.message);
         }
-      })
+      });
     };
+
     return {
       loginModalVisible,
       loginModalLoading,
@@ -119,16 +123,25 @@ export default defineComponent({
       loginUser,
       login,
       user,
-      logout,
+      logout
     }
   }
 });
 </script>
 
 <style>
+.logo {
+  width: 120px;
+  height: 31px;
+  /*background: rgba(255, 255, 255, 0.2);*/
+  /*margin: 16px 28px 16px 0;*/
+  float: left;
+  color: white;
+  font-size: 18px;
+}
 .login-menu {
+  float: right;
   color: white;
   padding-left: 10px;
-  float: right;
 }
 </style>
