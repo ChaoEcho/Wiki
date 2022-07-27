79fdb36cc54a007b7914c52b8c32507ca2e615f9
EchoChao <EchoChao@126.com>
2022-07-27 11:32:15 +0800

    9-12 前后端增加登录拦截

--- src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -1,21 +1,33 @@
-/*
 package com.jiawa.wiki.config;
 
-import com.jiawa.wiki.interceptor.LogInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.jiawa.wiki.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.Resource;
+
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-    @Autowired
-    LogInterceptor logInterceptor;
+    @Resource
+    LoginInterceptor loginInterceptor;
+
 
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**");
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
+
     }
 }
-*/
